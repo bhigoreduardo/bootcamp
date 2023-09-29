b44a473fef9c1ccf7c7585fae575c76bf2cbfbf0
bhigoreduardo <bhigoreduardo@gmail.com>
2023-09-29 10:52:09 -0300

    backup

---
+++ maps/nestjs/README.md
@@ -0,0 +1,10 @@
+- Prisma Init: npm install @prisma/client
+- Prisma Init: npx prisma init
+- Prisma Generate: npx prisma generate
+- Prisma Migrate: npx prisma migrate dev
+
+- Docker Up: docker compose up -d
+- Docker Up Build: docker compose up --build -d
+- Docker exec: docker exec -it maps-nestjs-1 bash
+- Docker compose exec: docker compose exec maps-nestjs bash
+- Docker compose logs: docker compose logs db
---
+++ maps/nestjs/src/routes/routes.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class RoutesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createRouteDto: CreateRouteDto) {
+    return this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: { name: 'source name', location: { lat: 0, lnt: 0 } },
+        destination: { name: 'destination name', location: { lat: 0, lnt: 0 } },
+        distance: 0,
+        duration: 0,
+        directions: '{}',
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.route.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.route.findUniqueOrThrow({
+      where: { id: id },
+    });
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
